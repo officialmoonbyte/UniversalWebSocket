9a2a74cbd2781f3b407d10e732810dc84d0756da
Alexander Ritter <braydelritter@gmail.com>
2018-09-10 15:23:47 -0500

    First commit

---
+++ UniversalWebSocket/UniversalWebSocket/Main.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Fleck;
+using IndieGoat.Net.Tcp;
+using IndieGoat.UniversalServer.Interfaces;
+
+namespace UniversalWebSocket
+{
+    public class UniversalWebSocket : IServerPlugin
+    {
+
+        #region Vars
+
+        WebSocketServer websocketserver;
+        int UniversalPort = 7777;
+
+        #region Default Interface Vars
+
+        public string Name
+        {
+            get { return "UniversalWebSocket"; }
+        }
+
+        public event EventHandler<SendMessageEventArgs> SendMessage;
+
+        #endregion Default Interface Vars
+
+        #endregion Vars
+
+        #region OnLoad
+
+        public void onLoad(string ServerDirectory)
+        {
+            int port = 7755;
+            string pluginDirectory = ServerDirectory + @"\WebSocketServer";
+            string PortFile = pluginDirectory + @"\Port.txt";
+            string UniversalPortFile = pluginDirectory + @"\UniversalPort.txt";
+
+            if (!Directory.Exists(pluginDirectory)) Directory.CreateDirectory(pluginDirectory);
+
+            if (File.Exists(PortFile))
+            {
+                port = int.Parse(File.ReadAllText(PortFile));
+            }
+            else
+            {
+                File.Create(PortFile).Close(); File.WriteAllText(PortFile, port.ToString());
+            }
+            if (File.Exists(UniversalPortFile))
+            {
+                UniversalPort = int.Parse(File.ReadAllText(UniversalPortFile));
+            }
+            else
+            {
+                File.Create(UniversalPortFile).Close(); File.WriteAllText(UniversalPortFile, UniversalPort.ToString());
+            }
+
+            websocketserver = new WebSocketServer("ws://127.0.0.1:" + port);
+            websocketserver.Start(socket =>
+            {
+                socket.OnOpen = () => Console.WriteLine("[WebSocketServer] Started WebSocket server on address ws://127.0.0.1:" + port);
+                socket.OnClose = () => Console.WriteLine("[WebSocketServer] WebSocket server stopped. May be error");
+                socket.OnMessage = message => OnMessageReceived(socket, message);
+            });
+        }
+
+        #endregion OnLoad
+
+        #region Invoke
+
+        public void Invoke(ClientSocketWorkload workload, ClientContext context, int port, List<string> Args, string ServerDirectory)
+        {
+            workload.SendMessage(context, "Cannot process your request.");
+        }
+
+        #endregion Invoke
+
+        #region Unload
+
+        public void Unload()
+        {
+            websocketserver.Dispose();
+        }
+
+        #endregion Unload
+
+        #region OnMessageReceived
+
+        private void OnMessageReceived(IWebSocketConnection socket, string Message)
+        {
+            UniversalClient client = new UniversalClient();
+            client.ConnectToRemoteServer(new UniversalConnectionObject("127.0.0.1", UniversalPort));
+
+            //string Messsage will be split by $%20 for command and &%20 for args
+
+            string command;
+
+            string[] ad1 = Message.Split(new string[] { "$%20" }, StringSplitOptions.RemoveEmptyEntries);
+            command = ad1[0];
+
+            string[] ad2 = ad1[1].Split(new string[] { "&%20" }, StringSplitOptions.RemoveEmptyEntries);
+
+            string response = client.ClientSender.SendCommand(command, ad2);
+            socket.Send(Encoding.ASCII.GetBytes(response));
+            client.Disconnect();
+            client = null;
+        }
+
+        #endregion OnMessageReceived
+
+    }
+}
---
+++ UniversalWebSocket/UniversalWebSocket/WebClient.js
@@ -0,0 +1,131 @@
+﻿/*
+ * Copyright (c) MoonByte Corporation 2018, MoonByte Group 2018
+ * 
+ * MoonByte Group contains the following - 
+ *  *Indie Goat
+ *  *Vortex Studio
+ *  *Shadow Goat Studio
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+//Initializing vars
+var isConnected = false;
+var websocket;
+
+/*
+ * The following functions are used to initialize connections
+ */
+
+function StartWebSocket(ServerIP, ServerPort)
+{
+    //Figure out if the browser supports WebSocket API
+    if (window.WebSocket == undefined)
+    {
+        //WebSockets is not supported, logs it in the console.
+        console.log("WebSockets is not supported!");
+    }
+    else
+    {
+        //WebSockets is supported, launches a new instance of WebSocket and add handlers functions on it
+        console.log("WebSockets is supported!")
+        OnLoad(ServerIP, ServerPort);
+    }
+}
+
+function OnLoad(ServerIP, ServerPort)
+{
+    //Creates a instance of the server addess to connect to. ws for non secure websockets.
+    //For future creations and updates to backend. We will update it to wss (ssl / https version of websockets)
+    var wsUri = "ws://" + ServerIP + ":" + ServerPort;
+
+    //Creates a new instance and connects to the generated uri
+    websocket = new WebSocket(wsUri);
+
+    //Adds all event handlers to the websocket
+    websocket.onopen = function (evt) { OnOpen(evt) };
+    websocket.onclose = function (evt) { OnClose(evt) };
+    websocket.onmessage = function (evt) { OnMessage(evt) };
+    websocket.onerror = function (evt) { OnError(evt) };
+}
+
+function OnOpen(evt)
+{
+    isConnected = truel
+    console.log("Websocket is connected to the server!");
+}
+
+/*
+ * The following functions are used to dispose of objects
+ */
+
+function OnClose(evt)
+{
+    //Log that the websocket is no-longer connected
+    isConnected = false;
+    console.log("WebSocket is currently not connected to the server!")
+}
+
+/*
+ * The following functions are used for messaging
+ */
+
+function AddMessage(Command, Args)
+{
+    //Format the message for the websocket client
+    var formatMessage = Command + "$%20" + Args.join('&%20');
+
+    if (isConnected == true) {
+        //Sends a message to the websocket
+        websocket.send(formatMessage);
+    } else {
+        //Sends a message to the websocket when the websocket opens
+        websocket.addEventListener('open', function (event) { websocket.send(formatMessage); });
+    }
+}
+
+function OnMessage(evt)
+{
+    //Gets the server data and initialize a new FileReader
+    serverMessage = evt.data;
+    reader = new FileReader();
+
+    //Attach a event to the reader for the load to end
+    reader.addEventListener('loadend', (e) =>
+    {
+        //Processes the reader result and display it in the console
+        console.log("Reader is done!");
+        const text = e.srcElement.result;
+        console.log(text);
+    });
+
+    //Log that the reader has started and start the reader.
+    console.log("Reader has started!");
+    reader.readAsText(serverMessage);
+}
+
+/*
+ * The following messages are used for error handling
+ */
+
+function OnError(evt)
+{
+
+}
